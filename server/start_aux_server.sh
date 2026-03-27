80c45bf3f1f2a6f375d5a995b321af142d68e51f
Caleb Simpson <caleb@simpson.center>
2026-03-27 07:27:59 -0400

    Set the bare repo HEAD to main after creation

--- server/start_aux_server.sh
+++ server/start_aux_server.sh
@@ -5,5 +5,6 @@ BARE_REPO="./bare_repo.git"
 if [ ! -d "$BARE_REPO" ] || ! git -C "$BARE_REPO" rev-parse --git-dir >/dev/null 2>&1; then
   echo "Initializing bare git repository in $BARE_REPO..."
   git init --bare "$BARE_REPO"
+  cd $BARE_REPO
+  git symbolic-ref HEAD refs/heads/main
 fi
-
